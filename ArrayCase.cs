b015f1c6c77b4c19cfc620ccef5a63bb3ad6e0ec
Harfianto <harfiantobinjeman@gmail.com>
2022-08-26 14:39:26 +0700

    add tes

---
+++ ArrayCase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    internal class ArrayCase
+    {
+        //declare array whig int data type
+        public static void TipyArray()
+        {
+            int[] arrint = new int[5];
+            arrint[0] = 12;
+            arrint[1] = 13;
+            arrint[2] = 14;
+            //arrint[6] = 15;
+
+            string[] listGril = new string[] {"yuli","Rini","Widi"};
+            foreach (var item in listGril)
+            {
+                Console.Write($"{item}");
+            }
+
+            for (int i = 0; i < listGril.Length; i++)
+            {
+                Console.Write($"{listGril[i]}");
+            }
+        }
+        //method reteun or function input array int, return array int
+        public static int[] InitArrayInt(int[] arr)
+        {
+            int[] arrint = arr;
+            return arrint;
+        }
+
+        //initial value with random
+
+        public static int[] InitArrayRandom(int n)
+        {
+            int[] arr = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(50);
+            }
+            return arr;
+        }
+        public static void DisplayArrayInt(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        //sum call element
+
+        public static double SumElement(int[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        public static int FindLargeElement(int[] arr)
+        {
+            int count = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > count)
+                {
+                    count = arr[i];
+                }
+            }
+            return count;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+*/
+
+//kalau malas mau buat consele
+
+using Day02;
+using System.Linq.Expressions;
+using static System.Console;
+
+//ArrayCase.TipyArray();
+
+//var myArray = ArrayCase.InitArrayInt(new int[] {10,20,30,40,50});
+//BIas Juga
+int[] n = { 1, 2, 3, 4, 5, 6, };
+var myArrays = ArrayCase.InitArrayInt(n);
+//ArrayCase.InitArrayInt(n);
+//ArrayCase.DisplayArrayInt(myArrays);
+var myRandom = ArrayCase.InitArrayRandom(20);
+ArrayCase.DisplayArrayInt(myRandom);
+Console.WriteLine($" Total Keseleuruhan Array : {ArrayCase.SumElement(myRandom)}");
+Console.WriteLine($"Angka Terbesar dari random Adalah : {ArrayCase.FindLargeElement(myRandom)}");
+
+int[] newArray = new int[myRandom.Length];
+
+
+var randomArray = new int[myRandom.Length];
+Array.Copy(myRandom, 0, randomArray, 0, myRandom.Length);
+Array.Reverse(randomArray);
+ArrayCase.DisplayArrayInt(randomArray);
