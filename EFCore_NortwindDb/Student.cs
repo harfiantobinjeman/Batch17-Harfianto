71a078724f276369ad59ee1c00daaa30e5080bb0
Harfianto <harfiantobinjeman@gmail.com>
2022-09-05 14:15:21 +0700

    configuration database dbnya

---
+++ EFCore_NortwindDb/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_NortwindDb
+{
+    public class Student
+    {
+        [Key]
+        public int studentId { get; set; }
+        [Required]
+        public string fullName { get; set; }
+
+        public int age { get; set; }
+    }
+}
